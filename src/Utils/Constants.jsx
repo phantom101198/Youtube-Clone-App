e9eca8f588724f8767f4805fe5bd02a6672eae76
harendra sharma <sharmaharendra60@gmail.com>
2025-11-24 08:03:03 +0530

    feat: update constants.jsx and add .env file to add API keys

--- src/Utils/Constants.jsx
+++ src/Utils/Constants.jsx
@@ -1,4 +1,2 @@
-// const API_KY = "AIzaSyDfbzaguO-wzPY0LtFpsHEndBBULIaPIkU";        // Key expired
-export const API_KEY = "AIzaSyAIi8VgLmgWhKlLLLRkRAGWCco6Nj2nY_I";
-// AIzaSyAIi8VgLmgWhKlLLLRkRAGWCco6Nj2nY_I
-export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&videoCategoryId=0&key=";
+export const API_KEY = import.meta.env.VITE_API_KEY;
+export const YOUTUBE_API = import.meta.env.VITE_YOUTUBE_API_KEY;
